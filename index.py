1ab9f9d007b6618e79697aa92033801846f870d5
Drake Axelrod <drake@draxel.io>
2022-03-19 22:20:56 +0100

    Update

---
+++ index.py
@@ -0,0 +1,37 @@
+from scripts.WhenIsGood import WhenIsGood
+
+wig = WhenIsGood("[2022-03-22 to 2022-03-27]")
+
+# drake
+drake = "Drake"
+wig.monday(drake, [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+wig.tuesday(drake, [17])
+wig.wednesday(drake, [8, 10])
+wig.thursday(drake, [14])
+wig.friday(drake, [])
+
+# anna
+anna = "Anna"
+wig.monday(anna, [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+wig.tuesday(anna, [17])
+wig.wednesday(anna, [8, 10])
+wig.thursday(anna, [14])
+wig.friday(anna, [])
+
+# gustav
+gustav = "Gustav"
+wig.monday(gustav, [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+wig.tuesday(gustav, [17])
+wig.wednesday(gustav, [8, 10])
+wig.thursday(gustav, [14])
+wig.friday(gustav, [])
+
+# vernita
+vernita = "Vernita"
+wig.monday(vernita, [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+wig.tuesday(vernita, [17])
+wig.wednesday(vernita, [8, 10])
+wig.thursday(vernita, [14])
+wig.friday(vernita, [])
+
+# wig.render()
